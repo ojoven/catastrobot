625ce49a656a1f80be8ac3fa876943412422c4d0
ojoven <mikeltorresmail@gmail.com>
2017-01-02 22:40:36 +0100

    Base created for property scrapping

--- functions.php
+++ functions.php
@@ -1,9 +1,16 @@
 <?php
 
-function curlPostViaShell($url, $params) {
+function curlPostViaShell($url, $params, $referer = false) {
 
 	$paramsBuilt = http_build_query($params);
-	$command = 'curl --data "' . $paramsBuilt . '" ' . $url;
+	$command = 'curl --data "' . $paramsBuilt . '"';
+
+	if ($referer) {
+		$command .= '--referer ' . $referer;
+	}
+
+	$command .= ' ' . $url;
+
 	$response = shell_exec($command);
 
 	return $response;
@@ -19,4 +26,8 @@ function getURLWithReferer($url, $referer) {
 	$context = stream_context_create($opts);
 	return file_get_contents($url, false, $context);
 
+}
+
+function _log($message) {
+	echo $message . PHP_EOL;
 }
