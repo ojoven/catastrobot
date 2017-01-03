075114c5c5360a25105bdd77f1632e55dc3fba06
ojoven <mikeltorresmail@gmail.com>
2017-01-03 10:39:26 +0100

    Property parsing

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ function curlPostViaShell($url, $params, $referer = false) {
 	$command = 'curl --data "' . $paramsBuilt . '"';
 
 	if ($referer) {
-		$command .= '--referer ' . $referer;
+		$command .= '--referer "' . $referer . '"';
 	}
 
 	$command .= ' ' . $url;
@@ -14,6 +14,25 @@ function curlPostViaShell($url, $params, $referer = false) {
 	$response = shell_exec($command);
 
 	return $response;
+}
+
+function curlPost($url, $params, $referer = false) {
+
+	$builtQuery = http_build_query($params);
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $builtQuery);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+
+	if ($referer) {
+		curl_setopt($ch, CURLOPT_REFERER, $referer);
+	}
+
+	$response = curl_exec($ch);
+	curl_close($ch);
+
+	return $response;
+
 }
 
 function getURLWithReferer($url, $referer) {
@@ -26,6 +45,12 @@ function getURLWithReferer($url, $referer) {
 	$context = stream_context_create($opts);
 	return file_get_contents($url, false, $context);
 
+}
+
+function writeToCSV($csv, $row) {
+	$handle = fopen($csv, "a");
+	fputcsv($handle, $row); # $line is an array of string values here
+	fclose($handle);
 }
 
 function _log($message) {
